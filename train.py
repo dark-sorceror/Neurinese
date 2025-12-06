c91f0c5f2b290f08441606d06531b0653234c770
Hao Yan <haoyan160@gmail.com>
2025-12-06 02:40:53 -0500

    Create training pipeline

---
+++ train.py
@@ -0,0 +1,68 @@
+import numpy as np
+from pathlib import Path
+from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
+
+from trainer import Trainer
+from model import CharacterRecognizer, CharacterDataset
+
+IMAGE_SIZE = 64
+NUM_CLASSES = 2
+
+DATA_ROOT = Path("./data")
+MODEL_SAVE_PATH = Path("./CNN_char_model.pth")
+
+def split_data(x, y, train = 0.8, val = 0.1):
+    x_train, x_temp, y_train, y_temp = train_test_split(
+        x, 
+        y, 
+        test_size = 1 - train, 
+        random_state = 42
+    )
+    
+    val_ratio = val / (1 - train)
+    
+    x_val, x_test, y_val, y_test = train_test_split(
+        x_temp, 
+        y_temp, 
+        test_size = 1 - val_ratio, 
+        random_state = 42
+    )
+    
+    return x_train, x_val, x_test, y_train, y_val, y_test
+
+if __name__ == "__main__":
+    try:
+        X_data = np.load(DATA_ROOT / "image.npy") 
+        Y_labels = np.load(DATA_ROOT / "label.npy")
+    except FileNotFoundError:
+        print("ERROR: Files not fouund")
+        
+        exit()
+        
+    x_train, x_val, x_test, y_train, y_val, y_test = split_data(X_data, Y_labels)
+
+    train_ds = CharacterDataset(x_train, y_train)
+    val_ds = CharacterDataset(x_val, y_val)
+    test_ds = CharacterDataset(x_test, y_test) 
+
+    train_loader = DataLoader(train_ds, batch_size=64, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_ds, batch_size=64, num_workers=4)
+    test_loader = DataLoader(test_ds, batch_size=64, num_workers=4)
+
+    model = CharacterRecognizer(num_classes = NUM_CLASSES)
+
+    trainer = Trainer(model) 
+
+    x, y = next(iter(train_loader))
+    print(x.shape)
+    print(x.min(), x.max()) 
+    
+    trainer.fit(
+        train_loader=train_loader,
+        val_loader=val_loader,
+        epochs=50,
+        checkpoint_path=MODEL_SAVE_PATH
+    )
+
+    print("Training complete.")
