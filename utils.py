ef32a71dba6c87b2193e7bddd9331fa572f67ea8
Hao Yan <haoyan160@gmail.com>
2026-01-29 22:50:30 -0700

    Debug Plotting

--- utils.py
+++ utils.py
@@ -8,16 +8,17 @@ def plot_strokes(seq: list[list[list[int]]], multiple: bool = True):
     
     for i in seq:
         x, y = 0.0, 0.0
-
+        
         for dx, dy, pen in i:
             nx, ny = x + dx, y + dy
-
-            if pen < 0.5:
+            
+            if pen > 0.5:
                 plt.plot([x, nx], [y, ny], 'k-', linewidth = 2)
-
+            else:
+                plt.plot([x, nx], [y, ny], color = '0.9', linewidth = 2)
+            
             x, y = nx, ny
 
     plt.gca().invert_yaxis()
     plt.axis('equal')
-    plt.axis('off')
     plt.show()
